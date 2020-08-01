290572b739e5593afbdb2d1eb3928bd4744430ec
shiasn <shiasn.ho@gmail.com>
2020-08-01 13:28:31 +0800

    checker

--- src/Plugin.js
+++ src/Plugin.js
@@ -1,3 +1,5 @@
+const DEFAULT_SPECIFIER_FLAG = 'ImportDefaultSpecifier';
+
 class Plugin {
   constructor (options, types) {
     this.t = types;
@@ -5,7 +7,29 @@ class Plugin {
     this.lazyDetected = false;
   }
 
-  ImportDeclaration (options) {}
+  ImportDeclaration (options) {
+    const { node } = options;
+    const { source } = node;
+
+    if (!source || !source.extra) return;
+
+    const extra = source.extra.rawValue;
+    const specifier = node.specifiers.find(({ type }) => type === DEFAULT_SPECIFIER_FLAG);
+
+    if (!specifier) return;
+
+    const local = specifier.local.name;
+
+    if (!this.check(local, extra)) {
+
+    }
+  }
+
+  check (local, extra) {
+    const { checker } = this.options;
+    if (!checker) return true;
+    return checker(local, extra);
+  }
 
   ProgramExit () {}
 }
